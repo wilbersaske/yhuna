2a398912cf7ee6e7aa190086d5872b1c3ddc964e
mantis-has <mantisbotmd@gmail.com>
2025-05-19 18:06:02 -0400

    Update gc-tagall.js

--- plugins/gc-tagall.js
+++ plugins/gc-tagall.js
@@ -5,8 +5,8 @@ const handler = async (m, {isOwner, isAdmin, conn, text, participants, args, com
     return;
   }
   const pesan = args.join` `;
-  const colombia = `💌 *Mensaje:* ${pesan}`;
-  let teks = `💥 *INVOCANDO GRUPO*\n${colombia}\n\n☁️ *Tags:*\n`;
+  const colombia = `💎 *Mensaje:* ${pesan}`;
+  let teks = `🩵 *MENSIONANDO AL GRUPO*\n${colombia}\n\n❦︎ *Miembros:*\n`;
   for (const mem of participants) {
     teks += `@${mem.id.split('@')[0]}\n`;
   }
