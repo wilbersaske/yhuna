b75c442deb7bd8a7be09e88f3ced11f33973f125
mantis-has <mantisbotmd@gmail.com>
2025-05-19 19:11:06 -0400

    Update rg-unreg.js

--- plugins/rg-unreg.js
+++ plugins/rg-unreg.js
@@ -3,7 +3,7 @@ let handler = async (m, { conn, text }) => {
 let user = global.db.data.users[m.sender]
 
 user.registered = false
-return conn.reply(m.chat, `*『✅』 Usted Ya No Está En Mi Base De Datos*`, m, rcanal)
+return conn.reply(m.chat, `*『✅』Tu registro fue borrado con éxito...*\n\n*Ya no podras usar mis comandos.*`, m, rcanal)
 
 }
 handler.help = ['unreg']
