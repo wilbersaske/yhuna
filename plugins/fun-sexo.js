df635a57af615bc92a2d47c25ae4476bbe847fa3
mantis-has <mantisbotmd@gmail.com>
2025-06-01 14:22:40 -0400

    Update prem-bots.js

--- plugins/fun-sexo.js
+++ plugins/fun-sexo.js
@@ -1,45 +1,243 @@
 import fs from 'fs';
 import path from 'path';
-import uploadImage from '../lib/uploadImage.js'
-import { sticker } from '../lib/sticker.js';
+import axios from 'axios';
 
 let handler = async (m, { conn, usedPrefix }) => {
-    let who;
-    if (m.isGroup) who = m.mentionedJid[0] ? m.mentionedJid[0] : m.quoted ? m.quoted.sender : false;
-    else who = m.chat;
-    if (!db.data.chats[m.chat].nsfw && m.isGroup) return m.reply('🚩 *¡Estos comandos están desactivados!*');
-    if (!who) throw 'Etiqueta o menciona a alguien';
+    let frases = [
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://qu.ax/SGDrw.jpg',
+            icono: 'https://qu.ax/uLhOy.jpg',
+            canal: 'https://whatsapp.com/channel/0029VbAOVajAO7RQt3rS683e'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://qu.ax/iUBoW.jpg',
+            icono: 'https://qu.ax/XRsZZ.jpg',
+            canal: 'https://whatsapp.com/channel/0029VbAOVajAO7RQt3rS683e'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://qu.ax/ldtsS.jpg',
+            icono: 'https://qu.ax/HLqhy.jpg',
+            canal: 'https://whatsapp.com/channel/0029VbAOVajAO7RQt3rS683e'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://qu.ax/R0v7T.jpg',
+            icono: 'https://qu.ax/tv36s.jpg',
+            canal: 'https://whatsapp.com/channel/0029VakLbM76mYPPFL0IFI3P'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://qu.ax/zEktf.png',
+            icono: 'https://qu.ax/eYQPF.jpeg',
+            canal: 'https://whatsapp.com/channel/0029VbAOVajAO7RQt3rS683e'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://qu.ax/NjfcJ.jpg',
+            icono: 'https://qu.ax/hKVCD.jpg',
+            canal: 'https://whatsapp.com/channel/0029VbAOVajAO7RQt3rS683e'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://qu.ax/5y0lM.jpg',
+            icono: 'https://qu.ax/m7e2Y.jpg',
+            canal: 'https://whatsapp.com/channel/0029VbAOVajAO7RQt3rS683e'
+        },
+        {
+            personaje: '♥️ 【 MENU PREM BOTS 】 ♥️',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://qu.ax/JETiZ.jpg',
+            icono: 'https://qu.ax/GLoHn.jpg',
+            canal: 'https://whatsapp.com/channel/0029VbAOVajAO7RQt3rS683e'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://qu.ax/dqRiC.png',
+            icono: 'https://qu.ax/tgqkZ.jpg',
+            canal: 'https://whatsapp.com/channel/0029VbAOVajAO7RQt3rS683e'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://qu.ax/eUzLi.jpg',
+            icono: 'https://qu.ax/uxSvb.png',
+            canal: 'https://whatsapp.com/channel/0029VbAOVajAO7RQt3rS683e'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+.',
+            imagen: 'https://qu.ax/zupOV.jpg',
+            icono: 'https://qu.ax/JbEnl.jpg',
+            canal: 'https://whatsapp.com/channel/0029VbAOVajAO7RQt3rS683e'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://qu.ax/CrNBM.jpg',
+            icono: 'https://qu.ax/OaBwM.jpg',
+            canal: 'https://whatsapp.com/channel/0029VbAOVajAO7RQt3rS683e'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://qu.ax/DKlAD.jpg',
+            icono: 'https://qu.ax/Ariqh.jpeg',
+            canal: 'https://whatsapp.com/channel/0029VbAOVajAO7RQt3rS683e'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://qu.ax/SnQfR.png',
+            icono: 'https://qu.ax/uUcYq.jpeg',
+            canal: 'https://whatsapp.com/channel/0029VbAOVajAO7RQt3rS683e'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'si no arriesgas tu vida, no puedes crear un futuro.',
+            imagen: 'https://files.catbox.moe/9ccgaf.jpg',
+            icono: 'https://files.catbox.moe/2mdcxf.jpg',
+            canal: 'https://whatsapp.com/channel/0029VbAOVajAO7RQt3rS683e'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://files.catbox.moe/42fduv.jpg',
+            icono: 'https://files.catbox.moe/v0nxvk.jpg',
+            canal: 'https://whatsapp.com/channel/0029VbAfd7zDDmFXm5adcF31'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://files.catbox.moe/e6qji2.png',
+            icono: 'https://files.catbox.moe/oc9zwf.jpg',
+            canal: 'https://whatsapp.com/channel/0029VbAfd7zDDmFXm5adcF31'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://files.catbox.moe/vtyjh0.jpg',
+            icono: 'https://files.catbox.moe/9rugzk.webp',
+            canal: 'https://whatsapp.com/channel/0029VbAfd7zDDmFXm5adcF31'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://files.catbox.moe/64f81b.jpg',
+            icono: 'https://files.catbox.moe/88xgpo.jpg',
+            canal: 'https://whatsapp.com/channel/0029VbAfd7zDDmFXm5adcF31'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://files.catbox.moe/kl0gim.jpg',
+            icono: 'https://files.catbox.moe/r4yx7z.jpg',
+            canal: 'https://whatsapp.com/channel/0029VbAa5sNCsU9Hlzsn651S'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://files.catbox.moe/izrj29.jpg',
+            icono: 'https://files.catbox.moe/zyrq8w.jpg',
+            canal: 'https://whatsapp.com/channel/0029VbAa5sNCsU9Hlzsn651S'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://files.catbox.moe/uhkl02.jpg',
+            icono: 'https://files.catbox.moe/vzykg3.jpg',
+            canal: 'https://whatsapp.com/channel/0029VbAa5sNCsU9Hlzsn651S'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://files.catbox.moe/8ukw2l.jpg',
+            icono: 'https://files.catbox.moe/g5bisg.jpg',
+            canal: 'https://chat.whatsapp.com/DEGsbJYf7BBDe8c7R2dUxU'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://files.catbox.moe/8ukw2l.jpg',
+            icono: 'https://files.catbox.moe/g5bisg.jpg',
+            canal: 'https://chat.whatsapp.com/DEGsbJYf7BBDe8c7R2dUxU'
+        },
+        {
+            personaje: '🩵 【 MENU PREM BOTS 】 🩵',
+            frase: 'Hola Soy Makima (Prem-Bot)\n\n╭━━━━━━━━━━\n┃Prem-Bots Makima\n┃Makima\n╰━━━━━━━━━━━━━\n\nAquí va a ir la lista de los comandos para Premium Bots de Makima\n\nLa versión premium está en proceso de desarrollo.\n\n> Made with Félix.
+',
+            imagen: 'https://qu.ax/nmpSD.jpeg',
+            icono: 'https://qu.ax/XPrwK.jpg',
+            canal: 'https://chat.whatsapp.com/DEGsbJYf7BBDe8c7R2dUxU'
+        }
+    ];
 
-    let user = global.db.data.users[who];
-    let name = conn.getName(who);
-    let name2 = conn.getName(m.sender);
-   // m.react('⏳');
-    await conn.sendMessage(m.chat, { react: { text: '🥵', key: m.key } })
-    let str = `${name2} tiene sexo con ${name}`.trim();
-    if (m.isGroup){
-    
-    // Directorio que contiene las imágenes
-    let pp = 'https://telegra.ph/file/3246f62c61a0ebebcb5c8.mp4'
-    let pp2 = 'https://telegra.ph/file/9c4b894e034c290df75e4.mp4'
-    let pp3 = 'https://telegra.ph/file/c5be4a906531c6731cd41.mp4'
-    let pp4 = 'https://telegra.ph/file/e3abb2e79cd1ccf709e91.mp4'
-    let pp5 = 'https://telegra.ph/file/a2ad1dd463a935d5dfd17.mp4'
-    let pp6 = 'https://telegra.ph/file/6f66fd1974e8df1496768.mp4'
-   let pp7 = 'https://telegra.ph/file/22d0ef801c93c1b2ac074.mp4'
-   let pp8 = 'https://telegra.ph/file/2072f260302c6bb97682a.mp4'
-    let pp9 = 'https://telegra.ph/file/820460f05d76bb2329bbc.mp4'
-    const videos = [pp, pp2, pp3, pp4, pp5, pp6, pp7, pp8, pp9];
-    const video = videos[Math.floor(Math.random() * videos.length)];
-    conn.sendMessage(m.chat, { video: { url: video }, gifPlayback: true, caption:str, mentions: [m.sender] },{ quoted: estilo })
-    };
-   
-   // m.react('🔥');
+    const elegido = frases[Math.floor(Math.random() * frases.length)];
+    let str = ` *${elegido.personaje}* dice:\n\n_"${elegido.frase}"_`;
+
+    // Descargar el icono como buffer
+    const thumb = await axios.get(elegido.icono, { responseType: 'arraybuffer' }).then(res => res.data);
+
+    m.react('🩵');
+
+    conn.sendMessage(m.chat, {
+        image: { url: elegido.imagen },
+        caption: str,
+        contextInfo: {
+            externalAdReply: {
+                mediaUrl: null,
+                mediaType: 3,
+                showAdAttribution: true,
+                title: elegido.personaje,
+                body: wm,
+                previewType: 0,
+                thumbnail: thumb,
+                sourceUrl: channel,
+            },
+            forwardedNewsletterMessageInfo: {
+                newsletterJid: '120363335626706839@newsletter',
+                newsletterName: 'Prem Bots Makima',
+                serverMessageId: '-1'
+            }
+        }
+    }, { quoted: m });
 }
 
-handler.help = ['sexo @tag'];
-handler.tags = ['fun'];
-handler.command = ['sexo']
-handler.register = true;
-handler.group = true;
+handler.help = ['menuprembots'];
+handler.tags = ['anime'];
+handler.command = ['fraseanime', 'phraseanime'];
 
 export default handler;
