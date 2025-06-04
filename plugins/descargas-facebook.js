5b01c3a46680f5d1dd46cc343165c2113fede600
mantis-has <mantisbotmd@gmail.com>
2025-06-04 10:33:19 -0400

    Update descargas-facebook.js

--- plugins/descargas-facebook.js
+++ plugins/descargas-facebook.js
@@ -2,40 +2,40 @@ import { igdl } from 'ruhend-scraper'
 
 const handler = async (m, { text, conn, args, usedPrefix, command }) => {
 if (!args[0]) {
-return conn.reply(m.chat, '🍟 *Ingresa Un Link De Facebook*', m, rcanal)}
+return conn.reply(m.chat, '🩵 *Ingresa Un enlace de Facebook*', m, rcanal)}
 let res
 try {
-conn.reply(m.chat, `🕒 *Descargando su video de facebook.*`, m, {
+conn.reply(m.chat, `🕒 *Descargando tú video de facebook.*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: '♡  ͜ ۬︵࣪᷼⏜݊᷼𝘿𝙚𝙨𝙘𝙖𝙧𝙜𝙖𝙨⏜࣪᷼︵۬ ͜ ',
-body: '<(✿◠‿◠)> 𝙈𝙚𝙜𝙪𝙢𝙞𝙣🔥',
+body: '🩵Félix Manuel🩵',
 previewType: 0, thumbnail: logo,
 sourceUrl: channel }}})
 await m.react(rwait)
 res = await igdl(args[0])
 } catch {
 await m.react(error)
-return conn.reply(m.chat, '🚩 *Error al obtener datos. Verifica el enlace.*', m, fake)}
+return conn.reply(m.chat, '🩵 ̸̷᮫໊᷐͢᷍ᰍ *Error al obtener datos. Verifica el enlace o tu falta de ortografía.*', m, fake)}
 let result = res.data
 if (!result || result.length === 0) {
-return conn.reply(m.chat, '🚩 *No se encontraron resultados.*', m, fake)}
+return conn.reply(m.chat, '🩵 *No se encontraron resultados de este enlace.*', m, fake)}
 let data
 try {
 await m.react(rwait)
 data = result.find(i => i.resolution === "720p (HD)") || result.find(i => i.resolution === "360p (SD)")
 } catch {
 await m.react(error)
-return conn.reply(m.chat, '🚩 *Error al procesar los datos.*', m, rcanal)}
+return conn.reply(m.chat, '🩵 *Error al procesar los datos.*', m, rcanal)}
 if (!data) {
-return conn.reply(m.chat, '🚩 *No se encontró una resolución adecuada.*', m, rcanal)}
+return conn.reply(m.chat, '🩵 *No se encontró una resolución adecuada.*', m, rcanal)}
 let video = data.url
 try {
 await m.react(rwait)
-await conn.sendMessage(m.chat, { video: { url: video }, caption: '🍟 *Tu video de facebook.*\n' + textbot, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: fkontak })
+await conn.sendMessage(m.chat, { video: { url: video }, caption: '🩵 *Aquí está tu video de Facebook.*\n' + textbot, fileName: 'fb.mp4', mimetype: 'video/mp4' }, { quoted: fkontak })
 await m.react(done)
 } catch {
 await m.react(error)
-return conn.reply(m.chat, '🚩 *Error al enviar el video.*', m, rcanal)}}
+return conn.reply(m.chat, '🩵 *Error al enviar el video.*', m, rcanal)}}
 
 handler.help = ['facebook', 'fb']
 handler.tags = ['descargas']
