b54077e9beb7d33a369a86db63162834c98b640f
mantis-has <mantisbotmd@gmail.com>
2025-06-04 12:08:52 -0400

    Update Maki-Bot

--- plugins/rpg-lb.js
+++ plugins/rpg-lb.js
@@ -10,17 +10,17 @@ let usersLevel = sortedLevel.map(enumGetKey)
 let len = args[0] && args[0].length > 0 ? Math.min(5, Math.max(parseInt(args[0]), 5)) : Math.min(5, sortedExp.length)
 
 let text = `
-╭───═[ *Top ${len} Chocolates 🍫* ]═────⋆
+╭───═[ *Top ${len} Diamantes* ]═────⋆
 │╭───────────────···
 ││ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-││ ${sortedLim.slice(0, len).map(({ jid, chocolates }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${chocolates} 🍫*`).join`\n││ `}
+││ ${sortedLim.slice(0, len).map(({ jid, chocolates }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${chocolates} 💎*`).join`\n││ `}
 │╰────────────────···
 ╰───────────═┅═──────────
 
-╭───═[ *TOP ${len} XP 💫* ]═────⋆
+╭───═[ *TOP ${len} XP ✨* ]═────⋆
 │╭───────────────···
 ││ Tú eres el *${usersLim.indexOf(m.sender) + 1}* de *${usersLim.length}*
-││ ${sortedExp.slice(0, len).map(({ jid, exp }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${exp} 💫*`).join`\n││ `}
+││ ${sortedExp.slice(0, len).map(({ jid, exp }, i) => `${i + 1}. ${participants.some(p => jid === p.jid) ? `(${conn.getName(jid)}) wa.me/` : '@'}${jid.split`@`[0]} *${exp} ✨*`).join`\n││ `}
 │╰────────────────···
 ╰───────────═┅═──────────
 
