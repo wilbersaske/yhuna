86f9a7f696bc02fdf9b8249182b16c9aef11a85a
mantis-has <mantisbotmd@gmail.com>
2025-06-04 10:41:39 -0400

    Update descargas-ig.js

--- plugins/descargas-ig.js
+++ plugins/descargas-ig.js
@@ -2,10 +2,10 @@ import { igdl } from "ruhend-scraper"
 
 let handler = async (m, { args, conn }) => { 
 if (!args[0]) {
-return conn.reply(m.chat, '🍟 *Ingresa un link de Instagram*', m, rcanal)}
+return conn.reply(m.chat, '🩵 *Porfavor, ingresa un link de Instagram*', m, rcanal)}
 try {
 await m.react(rwait)
-conn.reply(m.chat, `🕒 *Enviando El Video...*`, m, {
+conn.reply(m.chat, `🕒 *Procesando tu busqueda...*`, m, {
 contextInfo: { externalAdReply :{ mediaUrl: null, mediaType: 1, showAdAttribution: true,
 title: packname,
 body: wm,
@@ -15,10 +15,10 @@ let res = await igdl(args[0])
 let data = res.data       
 for (let media of data) {
 await new Promise(resolve => setTimeout(resolve, 2000))           
-await conn.sendFile(m.chat, media.url, 'instagram.mp4', '🍟 *Tu video de instagram.*\n' + textbot, fkontak)
+await conn.sendFile(m.chat, media.url, 'instagram.mp4', '🩵 *Aquí está tu video de instagram.*\n' + textbot, fkontak)
 }} catch {
 await m.react(error)
-conn.reply(m.chat, '🚩 Ocurrió un error.', m, fake)}}
+conn.reply(m.chat, '🩵 Ocurrió un error.', m, fake)}}
 
 handler.command = ['instagram', 'ig']
 handler.tags = ['descargas']
